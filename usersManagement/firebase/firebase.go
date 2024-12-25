3f85542d051aefd863da44d0fdccbee30f1e49e2
chostim <jostinalvaradosarmiento@gmail.com>
2024-12-24 20:38:34 -0500

    d

--- usersManagement/firebase/firebase.go
+++ usersManagement/firebase/firebase.go
@@ -1,21 +1,21 @@
 package firebase
 
 import (
-    "context"
-    "log"
+	"context"
+	"log"
 
-    "cloud.google.com/go/firestore"
-    "google.golang.org/api/option"
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/option"
 )
 
 var Client *firestore.Client
 
 func InitFirebase() {
-    ctx := context.Background()
-    sa := option.WithCredentialsFile("/home/chostim/Documents/proyectosDevEMp/taschedule/microservicios/usersManagement/config/key.json")
-    var err error
-    Client, err = firestore.NewClient(ctx, "glucobot-98eb2", sa)
-    if err != nil {
-        log.Fatalf("Failed to create Firestore client: %v", err)
-    }
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("key.json")
+	var err error
+	Client, err = firestore.NewClient(ctx, "task-444104", sa)
+	if err != nil {
+		log.Fatalf("Failed to create Firestore client: %v", err)
+	}
 }
