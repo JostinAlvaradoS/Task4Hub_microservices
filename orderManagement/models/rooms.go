fe9eaf8856449d18177e479f5e5b09d520598730
chostim <jostinalvaradosarmiento@gmail.com>
2025-01-16 13:34:11 -0500

    asd

--- orderManagement/models/rooms.go
+++ orderManagement/models/rooms.go
@@ -3,7 +3,6 @@ package models
 type Rooms struct {
 	ID          string      `json:"id"`
 	Type        string      `json:"type"`
-	Quantity    int         `json:"quantity"`
 	Observation Observation `json:"observation"`
 	Status      string      `json:"status"`
 	Evidence    Evidence    `json:"evidence"`
